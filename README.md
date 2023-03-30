4199be113796e52e7163922f41e84379ae43048c
Dahiru Ibrahim <suhayrid6@gmail.com>
2023-03-31 00:47:11 +0100

    Added FastAPI

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # TLDR-the-TnC
-TLDR the T&amp;C uses an LLM to understand the contents of Terms and Conditions documents and provides a user-friendly chatbot interface for users to ask questions and receive answers.
+> TLDR the T&amp;C is a Gradio-based chatbot that provides a user-friendly interface for users to ask questions about the contents of Terms and Conditions (T&C) documents. The chatbot uses an LLM (Language Model Machine) and the OpenAI API to understand the T&C documents and provide summarized answers to users' queries.
+
+> The code is designed to process both PDF and TXT files and includes functionality for splitting large documents into smaller chunks for more efficient processing. The OpenAI API is used for text embeddings and retrieval QA, which allows the chatbot to provide more accurate and relevant responses to users' questions.
+
+> Overall, TLDR-the-T&C provides a powerful and user-friendly way for users to quickly and easily understand the contents of T&C documents, without having to read through lengthy and often complicated legal documents themselves.
+
+# Built Using
+
+- Python
+- Langchain
+- OpenAI GPT-3.5 Turbo
+- Gradio
+
+# Usage
+
+``` 
+git clone https://github.com/Daheer/TLDR-the-TnC.git 
+pip install -r requirements.txt
+```
+> Paste your OpenAI API key in [OPENAI_API_KEY](OPENAI_API_KEY.txt)
+```
+uvicorn app:tldr_app
+```
+
+# Demo
+
+<code style="background-color:#a61237">live</code> demo available at [this Huggingface Space](https://huggingface.co/spaces/deedax/TLDR-the-TnC)
+
+# Reference
+
+[Langchain](https://langchain.readthedocs.io/)
+
+# Contact
+
+Dahir Ibrahim (Deedax Inc) - http://instagram.com/deedax_inc <br>
+Email - suhayrid@gmail.com <br>
+YouTube - https://www.youtube.com/channel/UCqvDiAJr2gRREn2tVtXFhvQ <br>
+Twitter - https://twitter.com/DeedaxInc <br>
+Project Link - https://github.com/Daheer/TLDR-the-TnC
---
+++ app.py
@@ -0,0 +1,122 @@
+import langchain
+import openai
+import os
+from langchain import OpenAI
+import langchain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain import OpenAI
+from langchain.document_loaders import DirectoryLoader
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+#import magic
+import nltk
+import time
+import re
+import gradio as gr
+from fastapi import FastAPI
+
+# Paste your OpenAI API key in the file OPENAI_API_KEY.txt (format: sk-XXZXXXXXXXXXXXXXX)
+
+with open('OPENAI_API_KEY.txt') as f:
+  OPENAI_API_KEY = f.read().strip()
+
+os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+
+TLDR_APP_PATH = "/tldr-bot"
+
+tldr_app = FastAPI()
+
+source_requests = [
+                   'source?',
+                   'source',
+                   ]
+
+class Engine:
+  title: str = ""
+  qa = None
+  source_document = None
+
+  def setup_file(self, filepath):
+    nltk.download('averaged_perceptron_tagger')
+    try:
+      loader = DirectoryLoader('/', glob = filepath[1:])
+      documents = loader.load()
+      text_splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap = 0)
+      texts = text_splitter.split_documents(documents)
+      embeddings = OpenAIEmbeddings(openai_api_key = os.getenv('OPENAI_API_KEY'))
+      docsearch = Chroma.from_documents(texts, embeddings)
+      chain_type_kwargs = {
+          "memory": ConversationBufferMemory()
+      }
+      llm = ChatOpenAI(temperature = 0, 
+                   verbose = True)
+      self.qa = RetrievalQA.from_chain_type(llm = llm, 
+                                           chain_type = 'stuff', 
+                                           retriever=docsearch.as_retriever(),
+                                           chain_type_kwargs = chain_type_kwargs, 
+                                           return_source_documents = True)
+      self.title = filepath
+    except:
+      raise Exception("Something went wrong when processing the txt file")  
+
+engine = Engine()
+
+def add_text(history, text):
+    history = history + [(text, None)]
+    return history
+
+def add_file(history, file):
+    engine.setup_file(file.name)
+    history = history + [("File succesfully uploaded. Prompt away! ✅", None)]
+    return history
+
+def bot(history, text):
+    if engine.qa:
+      response = engine.qa({'query': text})
+      if text.lower() in source_requests:
+        history[-1][1] = engine.source_document
+        yield history, ""
+      else:
+        history[-1][1] = ""
+        for info in re.split("(,|[\n\s+])", response['result']):
+          history[-1][1] += info
+          time.sleep(0.075)
+          engine.source_document = "\"" + response['source_documents'][0].page_content + " \""
+          yield history, ""
+    else:
+      history[-1][1] = "Upload a document first"
+      yield history, ""
+      
+with gr.Blocks(theme = gr.themes.Soft()) as demo:
+    chatbot = gr.Chatbot([], elem_id="chatbot", label = "TLDR the T&C").style(height = 750)
+
+    with gr.Row():
+        with gr.Column(scale=0.85):
+            txt = gr.Textbox(
+                show_label=False,
+                placeholder="Upload a T&C file (pdf or txt), then enter prompt",
+            ).style(container=False)
+        with gr.Column(scale=0.15, min_width=0):
+            btn = gr.UploadButton(f"📄", file_types=["text", "pdf"])
+
+    txt.submit(add_text, [chatbot, txt], [chatbot]).then(
+        bot, [chatbot, txt], [chatbot, txt]
+    )
+    btn.upload(add_file, [chatbot, btn], [chatbot])
+
+demo.queue().launch(share = True)
+tldr_app = gr.mount_gradio_app(tldr_app, demo, path=TLDR_APP_PATH)
+
+@tldr_app.get("/")
+def root():
+   return {
+        "response": "Welcome to the TLDR bot API"
+   }
+
